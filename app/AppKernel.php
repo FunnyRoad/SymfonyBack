0eb669ab3874e02e0b4d1da31c366c7fa9a156a0
Nassim <nassim.bouazabia@gmail.com>
2017-01-12 14:20:34 +0100

    Modification du gitignore et du dossier d'afficage des logs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,7 +40,7 @@ class AppKernel extends Kernel
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__).'/var/logs/my-project';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
