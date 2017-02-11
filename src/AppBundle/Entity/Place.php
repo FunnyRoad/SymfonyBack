f600ed584f52ca73f35c4dfa17f6c6b04ddbcbbf
Nassim <nassim.bouazabia@gmail.com>
2017-02-11 17:20:17 +0100

    Entite pour gerer les places à visiter

---
+++ src/AppBundle/Entity/Place.php
@@ -0,0 +1,146 @@
+<?php
+
+// src/AppBundle/Entity/Place.php
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="place")
+ */
+class Place {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $grade;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Place
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Place
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set grade
+     *
+     * @param float $grade
+     *
+     * @return Place
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    /**
+     * Get grade
+     *
+     * @return float
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+
+
+    function jsonSerialize()
+    {
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($this,'json');
+
+        return $jsonContent;
+    }
+
+
+}
