0a752c955714a98cbf76472ea8ccf7b444141396
Nassim <nassim.bouazabia@gmail.com>
2017-02-11 17:19:54 +0100

    Controller pour gérer les lieux à visiter

---
+++ src/AppBundle/Controller/PlaceController.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nassim
+ * Date: 12/01/17
+ * Time: 15:06
+ */
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Place;
+
+
+class PlaceController extends Controller{
+
+
+
+    /**
+     * @Route("/place/{id}", name="get_place")
+     * @Method("Get")
+     */
+    public function find($id){
+        //   echo "im in";
+
+        $em = $this->getDoctrine()->getManager();
+        $place = $em->find('AppBundle:Place',$id);
+        $em->flush();
+
+        return $this->json($place->jsonSerialize());
+
+    }
+
+
+    /**
+     * @Route("/place", name="get_plces")
+     * @Method("Get")
+     *
+     */
+    public function findAll(){
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+
+        $places = $this->getDoctrine()
+            ->getRepository('AppBundle:Place')
+            ->findAll();
+
+        $jsonContent = $serializer->serialize($places, 'json');
+
+        return $this->json($jsonContent);
+    }
+
+    /**
+     * @Route("/place", name="add_place")
+     * @Method("POST")
+     */
+    public function addPlace(Request $request){
+        //   echo "im in";
+
+        $params = array();
+        $content = $request->getContent();
+
+
+        if (!empty($content))
+        {
+            $params = json_decode($content, true); // 2nd param to get as array
+        }
+        $place = new Place();
+        $place->setDescription($params["description"]);
+        $place->setName($params["name"]);
+        $place->setgrade($params["grade"]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($place);
+        $em->flush();
+
+        return $this->json($place->jsonSerialize());
+    }
+
+    /**
+     * @Route("/place/{id}", name="place")
+     * @Method("DELETE")
+     */
+    public function deletePlace($id){
+        //   echo "im in";
+
+        $em = $this->getDoctrine()->getManager();
+        $place = $em->find('AppBundle:Place',$id);
+        $em->remove($place);
+        $em->flush();
+
+        return "heve been removed";
+
+    }
+
+
+
+    /**
+     * @Route("/place", name="update_place")
+     * @Method("PUT")
+     */
+    public function updatePlace(Request $request){
+        //   echo "im in";
+
+        $params = array();
+        $content = $request->getContent();
+
+
+        if (!empty($content))
+        {
+            $params = json_decode($content, true); // 2nd param to get as array
+        }
+        $place = new Place();
+        $place->setId($params["id"]);
+        $place->setDescription($params["description"]);
+        $place->setName($params["name"]);
+        $place->setgrade($params["grade"]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->merge($place);
+        $em->flush();
+
+        return $this->json($place->jsonSerialize());
+    }
+}
