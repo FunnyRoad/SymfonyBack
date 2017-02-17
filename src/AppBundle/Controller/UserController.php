575da279a546c025eafaf27e759a854840921afb
Nassim <nassim.bouazabia@gmail.com>
2017-02-17 02:58:41 +0100

    Ajout de l'entite User avec les methode CRUD

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nassim
+ * Date: 17/02/17
+ * Time: 02:37
+ */
+
+namespace AppBundle\Controller;
+
+
+class UserController
+{
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nassim
+ * Date: 17/02/17
+ * Time: 02:29
+ */
+
+namespace AppBundle\Entity;
+
+
+class User
+{
+
+}
