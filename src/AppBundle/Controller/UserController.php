2f9ea173bc3b3d08ce6c6dff38811dd2ee8490ea
Nassim <nassim.bouazabia@gmail.com>
2017-02-17 02:59:17 +0100

    Ajout de l'entite User avec les methodes CRUD

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,8 +8,128 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class UserController
+class UserController extends Controller
 {
+
+    /**
+     * @Route("/user/{id}", name="get_user")
+     * @Method("Get")
+     */
+    public function find($id){
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($id);
+        $em->flush();
+
+        return $this->json($user->jsonSerialize());
+
+    }
+
+
+    /**
+     * @Route("/user", name="get_users")
+     * @Method("Get")
+     *
+     */
+    public function findAll(){
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        $jsonContent = $serializer->serialize($users, 'json');
+
+        return $this->json($jsonContent);
+    }
+
+
+    /**
+     * @Route("/user", name="add_user")
+     * @Method("POST")
+     */
+    public function addUser(Request $request){
+
+        $params = array();
+        $content = $request->getContent();
+
+
+        if (!empty($content))
+        {
+            $params = json_decode($content, true); // 2nd param to get as array
+        }
+
+        $user = new User();
+
+        $user->setMail($params["mail"]);
+        $user->setFirebazeId($params["firebaseId"]);
+        $user->setFirtName($params["firstName"]);
+        $user->setLastName($params["lastName"]);
+        $user->setUsername($params["username"]);
+        $user->setBirthDate($params["birthDate"]);
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json($user->jsonSerialize());
+    }
+
+
+
+    /**
+     * @Route("/user/{id}", name="delete_user")
+     * @Method("DELETE")
+     */
+    public function deleteUser($id){
+        //   echo "im in";
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->find('AppBundle:User',$id);
+        $em->remove($user);
+        $em->flush();
+
+        return "heve been removed";
+
+    }
+
+
+    /**
+     * @Route("/user", name="update_user")
+     * @Method("PUT")
+     */
+    public function updateUser(Request $request){
+
+        $params = array();
+        $content = $request->getContent();
+
+        if (!empty($content))
+        {
+            $params = json_decode($content, true); // 2nd param to get as array
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($params["id"]);
+
+        $user->setMail($params["mail"]);
+        $user->setFirebazeId($params["firebaseId"]);
+        $user->setFirtName($params["firstName"]);
+        $user->setLastName($params["lastName"]);
+        $user->setUsername($params["username"]);
+        $user->setBirthDate($params["birthDate"]);
+
+
+        $em->flush();
+
+        return $this->json($user->jsonSerialize());
+    }
 
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,8 +8,184 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
-class User
+
+/**
+ * @ORM/Entity
+ * @ORM\Table(name="user")
+ */
+class User implements \JsonSerializable
 {
 
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer",nullable=false)
+     */
+    private $firebazeId;
+
+    /**
+     * @ORM\Column(type="string",nullable=false,length=50)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string",length=40)
+     */
+    private $firtName;
+
+    /*
+     * @ORM\Column(type="string",length=40)
+     */
+    private $lastName;
+
+    /**
+     * @ROM\Column(type="string",length=40)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthDate;
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirtName()
+    {
+        return $this->firtName;
+    }
+
+    /**
+     * @param mixed $firtName
+     */
+    public function setFirtName($firtName)
+    {
+        $this->firtName = $firtName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirebazeId()
+    {
+        return $this->firebazeId;
+    }
+
+    /**
+     * @param mixed $firebazeId
+     */
+    public function setFirebazeId($firebazeId)
+    {
+        $this->firebazeId = $firebazeId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param mixed $mail
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param mixed $birthDate
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize()
+    {
+        return [
+            "id"=>$this->id,
+            "firebazeId"=>$this->firebazeId,
+            "username"=>$this->username,
+            "mail"=>$this->mail,
+            "firstnName"=>$this->firtName,
+            "lastName"=>$this->lastName,
+            "birthDate"=>$this->birthDate
+        ];
+    }
 }
