184ab65b99ff4c2ab8b3373f7d3447375abde297
Nassim <nassim.bouazabia@gmail.com>
2017-02-24 20:54:20 +0100

    Controlleur pour gérer les invités dans un roadtrip

---
+++ src/AppBundle/Controller/GuestResources.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nassim
+ * Date: 22/02/17
+ * Time: 20:16
+ */
+
+namespace AppBundle\Controller;
+
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\User;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class GuestResources extends Controller
+{
+
+    /**
+     * @Route("/guest/{guestId}/roadtrips", name="get_user_roadtrip")
+     * @Method("Get")
+     */
+    public function findRoadTripsOfGuest($guestId){
+
+        $em = $this->getDoctrine()->getManager();
+        $guest = $em->getRepository('AppBundle:User')->find($guestId);
+
+        $respone = array();
+        foreach ($guest->getRoadtrips() as $roadtrip){
+            $respone[] = $roadtrip->jsonSerialize();
+        }
+        return $this->json($respone);
+    }
+
+    /**
+     * @Route("/roadtrip/{roadtripId}/guests", name="get_roadtrip_guests")
+     * @Method("Get")
+     */
+    public function findGuestsOfRoadtrip($roadtripId){
+
+        $em = $this->getDoctrine()->getManager();
+        $roadtrip = $em->getRepository('AppBundle:RoadTrip')->find($roadtripId);
+
+        $respone = array();
+        foreach ($roadtrip->getGuests() as $guest){
+            $respone[] = $guest->jsonSerialize();
+        }
+        return $this->json($respone);
+    }
+
+
+    /**
+     * @Route("/guest/{guestId}/roadTrip/{roadtripId}",name="add_guest_to_roadtrip")
+     * @Method("Put")
+     */
+    public function addGuestToRoadTrip($guestId,$roadtripId){
+        $em = $this->getDoctrine()->getManager();
+        $guest = $em->getRepository('AppBundle:User')->find($guestId);
+        $roadtrip = $em->getRepository('AppBundle:RoadTrip')->find($roadtripId);
+        $roadtrip->addguest($guest);
+        $em->flush();
+        return $this->json('"response":"guest have been added"');
+    }
+
+    /**
+     * @Route("/guest/{guestId}/roadtrip/{roadtripId}",name="remove_roadtip_guest")
+     * @Method("Delete")
+     */
+    public function removeGuestFromRoatrip($guestId,$roadtripId){
+        $em = $this->getDoctrine()->getManager();
+        $guest = $em->getRepository('AppBundle:User')->find($guestId);
+        $roadtrip = $em->getRepository('AppBundle:RoadTrip')->find($roadtripId);
+        $roadtrip->removeguest($guest);
+        $em->flush();
+        return $this->json('"response":"guest have been added"');
+    }
+
+}
