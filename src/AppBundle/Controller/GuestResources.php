c3dec58bbdbbeda1a6f7dfc6f357f93984e43777
Nassim <nassim.bouazabia@gmail.com>
2017-03-01 20:55:59 +0100

    Rien n'a changé, juste une methode de test

--- src/AppBundle/Controller/GuestResources.php
+++ src/AppBundle/Controller/GuestResources.php
@@ -13,17 +13,17 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use AppBundle\Entity\User;
-
-use Symfony\Component\HttpFoundation\Request;
-
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class GuestResources extends Controller
 {
+
+    /**
+     * @Route("test",name="test")
+     * @Method("get")
+     */
+    public function test(){
+        
+    }
 
     /**
      * @Route("/guest/{guestId}/roadtrips", name="get_user_roadtrip")
